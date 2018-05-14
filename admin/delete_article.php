b6d3b2b31a4c6224c047243f5f7c593fbac1b019
Champaloux Benjamin <tiitanyum@gmail.com>
2018-05-14 19:21:23 +0200

    presque

--- admin/delete_article.php
+++ admin/delete_article.php
@@ -5,6 +5,7 @@ session_start();
 require_once '../inc/connect.php';
 if (isset($_SESSION['admin']) AND !empty($_SESSION['admin'])) {
   if (isset($_GET['id'])) {
+    $req = $bdd->query('SELECT * FROM articles WHERE id = '.$_GET['id']);
     $article = $req->fetch();
     if ($article) {
       $req = $bdd->query('DELETE FROM articles WHERE id = '.$_GET['id']);
