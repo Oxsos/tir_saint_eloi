a72576e8029dc45fd1013a724d9c2829c89e6641
Champaloux Benjamin <tiitanyum@gmail.com>
2018-05-14 19:18:49 +0200

    calme reste calme
    This reverts commit 49341fdb94d6a68717b8bc49845e5a0bfe7232d3, reversing
    changes made to 65a1663b9e0e0c1459e5468947db427a2894594f.

--- admin/delete_article.php
+++ admin/delete_article.php
@@ -5,10 +5,6 @@ session_start();
 require_once '../inc/connect.php';
 if (isset($_SESSION['admin']) AND !empty($_SESSION['admin'])) {
   if (isset($_GET['id'])) {
-<<<<<<< HEAD
-    $req = $bdd->query('SELECT * FROM articles WHERE id = '.$_GET['id'])
-=======
->>>>>>> fdd72ca407fff097bf615c7d050ab722d082c78a
     $article = $req->fetch();
     if ($article) {
       $req = $bdd->query('DELETE FROM articles WHERE id = '.$_GET['id']);
