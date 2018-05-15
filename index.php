8a89f6d83b0b763d88f5c4346d48cc60dbf84184
Champaloux Benjamin <tiitanyum@gmail.com>
2018-05-15 20:55:10 +0200

    css again

--- index.php
+++ index.php
@@ -7,37 +7,39 @@
   </head>
 
 <//----------------------------------------------------header------------------------------------------------------->
-
+  <?php require_once "inc/connect.php"; ?>
   <body>
 
 <//----------------------------------------------------Articles------------------------------------------------------->
+
+    <div class="article-cont">
+
     <?php
-      require_once "inc/connect.php";
-      $req = $bdd->query('SELECT * FROM articles ORDER BY id DESC LIMIT 30');
-      $article = $req->fetchAll();
+    $req = $bdd->query('SELECT * FROM articles ORDER BY id DESC LIMIT 30');
+    $article = $req->fetchAll();
 
-      foreach ($article as $article):     ?>
+    foreach ($article as $article):   ?>
 
-      <div class="articles">
-        <div class="article">
-          <div class="img-article">
-            <img src="http://via.placeholder.com/350x150"><br>
-            <h3><?= $article['name']?></h3>
-          </div><br>
-          <div class="content-article">
-            <?php echo substr($article['content'], 0, 400);
-             ?>
-            <br>
-            <br>
-            Par: <?= $article['Autor'] ?>
-          </div><br>
-          <div class="link-article">
-            <a href="gestion_article.php?id=<?= $article['id'] ?>">Voir l'article / le modifier</a>
-          </div>
+      <div class="article">
+
+        <div class="img-article">
+          <img src="http://via.placeholder.com/350x150"><br>
+          <h3><?= $article['name']?></h3>
+        </div>
+
+        <div class="content-article">
+          <?php echo substr($article['content'], 0, 400);
+           ?>
+          <br>
+          Par: <?= $article['Autor'] ?>
+        </div>
+
+        <div class="link-article">
+          <a href="gestion_article.php?id=<?= $article['id'] ?>">Voir l'article / le modifier</a>
         </div>
       </div>
-
     <?php endforeach ?>
+    </div>
 <//----------------------------------------------------footer------------------------------------------------------->
 
 
